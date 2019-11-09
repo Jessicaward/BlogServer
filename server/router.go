286df4ce760e0223b66926517240fda70b8b404c
Jessica Ward <Jessicawardiom@gmail.com>
2019-11-09 11:28:47 +0000

    Added test route to load and deliver file
    Doesn't compile yet, it cannot find `BlogServer/main` at the gopath

--- server/router.go
+++ server/router.go
@@ -3,11 +3,22 @@ package server
 import (
 	"net/http"
 	"fmt"
+	"BlogServer/main"
 )
 
 func HandleRoute(){
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Welcome to my site")
+	})
+
+	http.HandleFunc("/blog/", func (w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Welcome to my blog")
+	})
+
+	post := pageBuilder.BuildPage("/posts/testpost.html")
+	//todo: test route, remove
+	http.HandleFunc("/test/", func (w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, )
 	})
 
 	http.ListenAndServe(":80", nil)
