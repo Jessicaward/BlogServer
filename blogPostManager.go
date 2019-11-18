d0c2e3d26da8c9c0a1fd67d7abf343eeeef24913
Jessica Ward <Jessicawardiom@gmail.com>
2019-11-18 17:45:30 +0000

    Added (broken) data access code

--- blogPostManager.go
+++ blogPostManager.go
@@ -3,33 +3,46 @@ package main
 import (
 	// Private
 	"BlogServer/utilities"
-	"strings"
+	"database/sql"
 
 	//Public
 	"gopkg.in/russross/blackfriday.v2"
 )
 
 type BlogPost struct {
-	Title string
-	Body  string
+	Metadata Post
+	Body     string
+}
+
+type Post struct {
+	Title     string
+	CreatedAt string
+	Tags      []Tag
+}
+
+type Tag struct {
+	Key  string
+	Name string
 }
 
 func GetPost(postName string) BlogPost {
 	post := utilities.ReadFile("posts/" + postName + ".md")
 	postBody := generateHtmlFromMarkdown(post)
-	postTitle := getTitleFromMarkdown(post)
+	metadata := new(Post)
+	//open database connection
+	database, _ := sql.Open("sqlite3", "./nraboy.db")
+
+	//todo: finish this
+	rows, _ := database.Query("SELECT Title, CreatedAt FROM Post")
+	for rows.Next() {
+		rows.Scan(post.Title, post.CreatedAt)
+	}
 
 	return BlogPost{
-		Body:  string(postBody),
-		Title: postTitle,
+		Body: string(postBody),
 	}
 }
 
 func generateHtmlFromMarkdown(markdown []byte) []byte {
 	return blackfriday.Run(markdown)
-}
-
-func getTitleFromMarkdown(markdown []byte) string {
-	//todo: this returns the title + the remainder of the post
-	return strings.Trim(string(markdown), " #")
 }
