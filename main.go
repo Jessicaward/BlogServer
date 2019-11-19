249e441df3b73f7b8614b8b96a94fd785e5773e1
Jessica Ward <Jessicawardiom@gmail.com>
2019-11-19 23:00:55 +0000

    Reverting test code in main.go

--- main.go
+++ main.go
@@ -5,10 +5,6 @@ import (
 )
 
 func main() {
-	var blog = GetPost("test-post")
-	fmt.Println("title: " + blog.Metadata.Title)
-	fmt.Println("created at: " + blog.Metadata.CreatedAt)
-	fmt.Println("body: " + blog.Body)
-	//fmt.Println("HTTP Server starting...")
-	//HandleRoute()
+	fmt.Println("HTTP Server starting...")
+	HandleRoute()
 }
